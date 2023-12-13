3df098f20017dbf69318464755e290ecba13fa40
meetp07 <meetpatelg3@gmail.com>
2023-12-13 19:22:44 +0530

    Update user profile forms and views (generated by blackbox.ai )

--- Project/Blog/models.py
+++ Project/Blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content=models.TextField()
+    date_posted=models.DateTimeField(default=timezone.now)
+    # will set current time, if you want to set a time when post was created then auto_now_add=True, you will not be able to change the time.
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    #will delete the post if author is deleted, but doesn't delete the author if post is deleted.
+
+    def __str__(self):
+        return self.title
--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Blog.apps.BlogConfig',
+    'Users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,7 +122,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
